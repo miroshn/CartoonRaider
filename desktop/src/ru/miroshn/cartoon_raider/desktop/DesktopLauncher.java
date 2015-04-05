71ca8f564cac7b50e30cfc60e1cbdc97f79bea6a
miroshn <miroshn11@yandex.ru>
2015-04-05 13:19:35 +0300

    Initial commit

---
+++ desktop/src/ru/miroshn/cartoon_raider/desktop/DesktopLauncher.java
@@ -0,0 +1,18 @@
+package ru.miroshn.cartoon_raider.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.miroshn.cartoon_raider.CartoonRaider;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.resizable = false;
+//		config.width = 1024;
+//		config.height = 768;
+		config.width = 400;
+		config.height = 400;
+
+		new LwjglApplication(new CartoonRaider(), config);
+	}
+}
