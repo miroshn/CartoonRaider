e2f22ac33cfbde89833694b7a7b9c038419fc95f
miroshn <miroshn11@yandex.ru>
2015-04-06 14:10:24 +0300

    предварительная заливка фона изменена с черной на белую

--- core/src/ru/miroshn/cartoon_raider/screens/GameScreen.java
+++ core/src/ru/miroshn/cartoon_raider/screens/GameScreen.java
@@ -40,7 +40,7 @@ public class GameScreen implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClearColor(1, 1, 1, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         leftUpCorner.x = leftUpCorner.y = 0;
