36f67034de44d6647af295a0c8fe5b7f1a202f61
miroshn <miroshn11@yandex.ru>
2015-04-09 10:18:10 +0300

    сделан постоянный скролл фона

--- core/src/ru/miroshn/cartoon_raider/gameobjects/Background.java
+++ core/src/ru/miroshn/cartoon_raider/gameobjects/Background.java
@@ -5,29 +5,40 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.actions.MoveToAction;
+import com.badlogic.gdx.scenes.scene2d.actions.MoveByAction;
 import com.badlogic.gdx.utils.Disposable;
 
 /**
  * Created by miroshn on 08.04.15.
+ * фон игры
  */
 public class Background extends Actor implements Disposable {
-    Texture texture;
-    TextureRegion region;
-    MoveToAction action;
+    private Texture texture;
+    private TextureRegion region;
+    private MoveByAction action;
+    private int SPEED_SCROLL = 5;
 
     public Background() {
         texture = new Texture("background.jpg");
         region = new TextureRegion(texture);
         setPosition(0, 0);
         setSize(texture.getWidth(), texture.getHeight());
+        this.addAction(createAction());
+
+    }
+
+    private MoveByAction createAction() {
+        action = new MoveByAction();
+        action.setAmountY(-texture.getHeight());
+        action.setDuration(SPEED_SCROLL);
+        return action;
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
         for (int x = 0; x < Gdx.graphics.getWidth(); x += texture.getWidth()) {
-            for (int y = 0; y < Gdx.graphics.getHeight(); y += texture.getHeight()) {
+            for (int y = 0; y < Gdx.graphics.getHeight() * 2; y += texture.getHeight()) {
                 batch.draw(region, getX() + x, getY() + y, getWidth(), getHeight(), getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
             }
         }
@@ -36,11 +47,12 @@ public class Background extends Actor implements Disposable {
     @Override
     public void act(float delta) {
         super.act(delta);
-        if (getActions().size == 0) {
-            action = new MoveToAction();
-            action.setPosition(getX(), -texture.getHeight());
-            action.setDuration(2);
-            addAction(action);
+//        Gdx.app.log("Debug", "Actions size = " + getActions().size);
+        if (getY() <= -getHeight()) {
+            setPosition(0, 0);
+
+            this.removeAction(action);
+            this.addAction(createAction());
         }
     }
 
