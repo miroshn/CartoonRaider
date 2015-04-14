4e000c48fdd095c330c9288a7b75873d0239dfb0
miroshn <miroshn11@yandex.ru>
2015-04-14 18:26:53 +0300

    Исправлены координаты появления вражин и их конечных точек движения

--- core/src/ru/miroshn/cartoon_raider/gameobjects/EnemyIstrebitel.java
+++ core/src/ru/miroshn/cartoon_raider/gameobjects/EnemyIstrebitel.java
@@ -51,10 +51,11 @@ public class EnemyIstrebitel extends GameObject implements Disposable {
 
         if (getY() < -100) {
             this.init();
-            setPosition(rnd.nextInt(Gdx.graphics.getWidth()) + getWidth() * getScaleX(),
+            setPosition(rnd.nextInt(Gdx.graphics.getWidth() - (int) (getWidth() * getScaleX())) + getWidth() * getScaleX(),
                     Gdx.graphics.getHeight() + getHeight() + rnd.nextInt(300));
+            Gdx.app.log("Reinit EnemyIstrebitel", "x=" + (Gdx.graphics.getWidth() - (int) (getWidth() * getScaleX())));
             clearActions();
-            addAction(Actions.moveTo(rnd.nextInt(Gdx.graphics.getWidth()) + getWidth() * getScaleX(), -200, (rnd.nextInt(100) + 50) / 10.f));
+            addAction(Actions.moveTo(rnd.nextInt(Gdx.graphics.getWidth() - (int) (getWidth() * getScaleX())) + getWidth() * getScaleX(), -200, (rnd.nextInt(100) + 50) / 10.f));
         }
     }
 
