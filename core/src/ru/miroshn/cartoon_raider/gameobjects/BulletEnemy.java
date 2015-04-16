779387dffe34b95218d42fb6c506b2b17496129a
miroshn <miroshn11@yandex.ru>
2015-04-16 12:06:19 +0300

    Снаряд выпускаемый противниками

---
+++ core/src/ru/miroshn/cartoon_raider/gameobjects/BulletEnemy.java
@@ -0,0 +1,29 @@
+package ru.miroshn.cartoon_raider.gameobjects;
+
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.utils.Array;
+
+/**
+ * Created by miroshn on 16.04.15.
+ * Снаряд выпущенный вражиной
+ */
+public class BulletEnemy extends Bullet {
+    @Override
+    public void act(float delta) {
+        if (getY() < -getHeight())
+            setState(GOState.DEAD);
+        if (this.getState() == GOState.DEAD)
+            getStage().getActors().removeValue(this, true);
+        super.act(delta);
+        Array<Actor> actors = getStage().getActors();
+        for (Actor a : actors) {
+            if (a instanceof Istrebitel) {
+                if (((GameObject) a).getBoundingPolygon().overlaps(this.getBoundingPolygon()) && ((GameObject) a).getState() == GOState.NORMAL) {
+                    ((GameObject) a).damageDeal(getDamagePower());
+                    this.setState(GOState.DEAD);
+                }
+            }
+        }
+
+    }
+}
