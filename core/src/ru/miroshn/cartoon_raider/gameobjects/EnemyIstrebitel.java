3945efec83a0c5ab135279bfa986d43bbe56821d
miroshn <miroshn11@yandex.ru>
2015-04-20 07:59:34 +0300

    Снаряд игрока наносит урон вражескому истребителю, сам при этом погибает

--- core/src/ru/miroshn/cartoon_raider/gameobjects/EnemyIstrebitel.java
+++ core/src/ru/miroshn/cartoon_raider/gameobjects/EnemyIstrebitel.java
@@ -55,6 +55,7 @@ public class EnemyIstrebitel extends GameObject implements Disposable {
                 gameObject.contact(this);
                 break;
             case PLAYER_BULLET:
+                gameObject.contact(this);
                 break;
             default:
         }
--- core/src/ru/miroshn/cartoon_raider/gameobjects/PlayerBullet.java
+++ core/src/ru/miroshn/cartoon_raider/gameobjects/PlayerBullet.java
@@ -1,8 +1,6 @@
 package ru.miroshn.cartoon_raider.gameobjects;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.utils.Array;
 
 /**
  * Created by miroshn on 16.04.15.
@@ -17,15 +15,15 @@ public class PlayerBullet extends Bullet {
         if (this.getState() == GOState.DEAD)
             getStage().getActors().removeValue(this, true);
         super.act(delta);
-        Array<Actor> actors = getStage().getActors();
-        for (Actor a : actors) {
-            if (a instanceof GameObject && !(a instanceof Bullet) && !(a instanceof Istrebitel)) {
-                if (((GameObject) a).getBoundingPolygon().overlaps(this.getBoundingPolygon()) && ((GameObject) a).getState() == GOState.NORMAL) {
-                    ((GameObject) a).damageDeal(getDamagePower());
-                    this.setState(GOState.DEAD);
-                }
-            }
-        }
+//        Array<Actor> actors = getStage().getActors();
+//        for (Actor a : actors) {
+//            if (a instanceof GameObject && !(a instanceof Bullet) && !(a instanceof Istrebitel)) {
+//                if (((GameObject) a).getBoundingPolygon().overlaps(this.getBoundingPolygon()) && ((GameObject) a).getState() == GOState.NORMAL) {
+//                    ((GameObject) a).damageDeal(getDamagePower());
+//                    this.setState(GOState.DEAD);
+//                }
+//            }
+//        }
 
     }
 
@@ -36,6 +34,15 @@ public class PlayerBullet extends Bullet {
 
     @Override
     public void contact(GameObject gameObject) {
-
+        switch (gameObject.who()) {
+            case ENEMY_ISTREBITEL:
+                if (getState() != GOState.NORMAL) break;
+                if (gameObject.getState() != GOState.NORMAL) break;
+                gameObject.damageDeal(getDamagePower());
+                this.setState(GOState.DEAD);
+                this.clearActions();
+                break;
+            default:
+        }
     }
 }
