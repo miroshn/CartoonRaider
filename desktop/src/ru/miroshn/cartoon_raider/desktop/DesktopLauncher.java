a18936f038ce6ec5aeb7bc30b60e62c89aa10add
miroshn <miroshn11@yandex.ru>
2015-04-21 13:27:22 +0300

    test commit

--- desktop/src/ru/miroshn/cartoon_raider/desktop/DesktopLauncher.java
+++ desktop/src/ru/miroshn/cartoon_raider/desktop/DesktopLauncher.java
@@ -8,7 +8,7 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.resizable = false;
-//		config.width = 320;
+///		config.width = 320;
 //		config.height = 480;
 		config.width = 320 * 2;
 		config.height = 480 * 2;
