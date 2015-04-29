90b9b5924d20aeb76f788bf8c580d01944bace32
miroshn <miroshn11@yandex.ru>
2015-04-29 15:09:26 +0300

    В начале игры было 2 снаряда - исправлено

--- core/src/ru/miroshn/cartoon_raider/gameobjects/Istrebitel.java
+++ core/src/ru/miroshn/cartoon_raider/gameobjects/Istrebitel.java
@@ -114,7 +114,7 @@ public class Istrebitel extends GameObject implements Disposable {
         intAction = new IntAction();
         intAction.setValue(100);
         setTextureRegion(new TextureRegion((Texture) CRAssetManager.getInstance().get("istrebitel1.png")));
-        bulletLevel = 2;
+        bulletLevel = 1;
         speedBulletFire = MIN_ROF;
         super.init();
     }
