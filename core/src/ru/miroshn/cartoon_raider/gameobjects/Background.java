006619b03dc8373f1b6fc9faf76073bc3393c438
miroshn <miroshn11@yandex.ru>
2015-06-01 16:16:07 +0300

    Добавленны комментарии и javadoc

--- core/src/ru/miroshn/cartoon_raider/gameobjects/Background.java
+++ core/src/ru/miroshn/cartoon_raider/gameobjects/Background.java
@@ -10,48 +10,70 @@ import ru.miroshn.cartoon_raider.helpers.CRAssetManager;
 import ru.miroshn.cartoon_raider.helpers.Res;
 
 /**
+ * Фон игры, так как используется на многих экранах и присутствует всегда - сделан в виде синглтона <br/>
  * Created by miroshn on 08.04.15.
- * фон игры
  */
 public class Background extends Actor implements Disposable {
-    private static Background instance;
-    private final TextureRegion region;
-    private MoveByAction action;
-    private int SPEED_SCROLL = 5;
+    private static Background instance; // экземпляр класса
+    private final TextureRegion backgroundTR; // текстура фона
+    private MoveByAction action; // постоянное действие для фона - смещение
+    private int SPEED_SCROLL = 5; // скорость перемещения фона
 
 
+    /**
+     * Конструктор, подготовка текстуры, установка размера и масштаба в зависимости от DPI экрана
+     */
     private Background() {
-        region = CRAssetManager.getInstance().get(Res.BACKGROUND);
+        backgroundTR = CRAssetManager.getInstance().get(Res.BACKGROUND);
         setPosition(0, 0);
-        setSize(region.getRegionWidth(), region.getRegionHeight());
+        setSize(backgroundTR.getRegionWidth(), backgroundTR.getRegionHeight());
         if (Gdx.graphics.getDensity() > 1)
             setScale(Gdx.graphics.getDensity());
         this.addAction(createAction());
 
     }
 
+    /**
+     * Получить ссылку на экземпляр класса
+     *
+     * @return Экземпляр класса Background
+     */
     public static Background getInstance() {
         if (instance == null) instance = new Background();
         return instance;
     }
 
+    /**
+     * настройка движения фона
+     * @return Объект сдвига
+     */
     private MoveByAction createAction() {
         action = new MoveByAction();
-        action.setAmountY(-region.getRegionHeight());
+        action.setAmountY(-backgroundTR.getRegionHeight());
         action.setDuration(SPEED_SCROLL);
         return action;
     }
 
+    /**
+     * Отрисовка фона, фон застилается по горизонтали и вертикали в стык, пока не будет заполнен весь видимый экран
+     * @param batch упаковщик с помошью которого рисуется объект
+     * @param parentAlpha прозрачность
+     */
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
-        for (int x = 0; x < Gdx.graphics.getWidth(); x += region.getRegionWidth()) {
-            for (int y = 0; y < Gdx.graphics.getHeight() * 2; y += region.getRegionHeight()) {
-                batch.draw(region, getX() + x, getY() + y, getWidth(), getHeight(), getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
+        for (int x = 0; x < Gdx.graphics.getWidth(); x += backgroundTR.getRegionWidth()) {
+            for (int y = 0; y < Gdx.graphics.getHeight() * 2; y += backgroundTR.getRegionHeight()) {
+                batch.draw(backgroundTR, getX() + x, getY() + y, getWidth(), getHeight(), getWidth(), getHeight(), getScaleX(), getScaleY(), getRotation());
             }
         }
     }
 
+    /**
+     * Обсчет логики фона, если элемент фона ушел за пределы экрана он перемещается обратно, при этом обновляется
+     * действие движения, в итоге получается плавное, постоянное движение фона
+     * @param delta время прошедшее от предыдущего пересчета кадра
+     */
     @Override
     public void act(float delta) {
         super.act(delta);
@@ -64,6 +86,9 @@ public class Background extends Actor implements Disposable {
         }
     }
 
+    /**
+     * Уничтожение синглтона, его необходимо проводить обязательно, иначе появляются проблемы на мобильных устройствах
+     */
     @Override
     public void dispose() {
 //        texture.dispose();
