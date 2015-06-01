1ef92b9f3a792d8182e6a8b8731d8d11bd95a7b9
miroshn <miroshn11@yandex.ru>
2015-06-01 15:39:42 +0300

    Добавленны комментарии и javadoc

--- core/src/ru/miroshn/cartoon_raider/gameobjects/ui/IstrebitelButton.java
+++ core/src/ru/miroshn/cartoon_raider/gameobjects/ui/IstrebitelButton.java
@@ -8,25 +8,40 @@ import ru.miroshn.cartoon_raider.helpers.CRAssetManager;
 import ru.miroshn.cartoon_raider.helpers.Res;
 
 /**
- * Created by CAHEK on 11.04.2015.
- * кнопка с изображением истребителя и текстом под ней
+ * Кнопка с изображением истребителя и текстом под ней, используется в главном меню программы
+ * Автор miroshn on 11.04.2015.
  */
 public class IstrebitelButton extends Istrebitel {
-    private final String text;
-    private final BitmapFont font;
+    private final String text;  // подпись к кнопке
+    private final BitmapFont font; // шрифт которым выводится подпись
 
+    /**
+     * Конструктор, использует шрифт загруженный менеджером ресурсов
+     *
+     * @param text Надпись под кнопкой
+     * @see CRAssetManager
+     */
     public IstrebitelButton(String text) {
         super();
         this.text = text;
         font = CRAssetManager.getInstance().get(Res.FONT);
     }
 
-
+    /**
+     * Метод заглушка, используется для подавления логики работы класса Istrebitel, если его не будет, или будет вызван
+     * super.act(delta); кнопка начнет стрелять и вести себя не как кнопка..
+     * @param delta время прошедшее с предыдущего вызова метода act(delta) - в данном случае не используется
+     */
     @Override
     public void act(float delta) {
 //        super.act(delta);
     }
 
+    /**
+     * Отрисовка кнопки и текста под ней
+     * @param batch Упаковщик с помощью которого идет отрисовка
+     * @param parentAlpha Прозрачность - не используется
+     */
     @Override
     public void draw(Batch batch, float parentAlpha) {
         super.draw(batch, parentAlpha);
@@ -36,8 +51,4 @@ public class IstrebitelButton extends Istrebitel {
         font.draw(batch, text, getX() + getWidth() * getScaleX() / 2 - tb.width / 2, getY() - 10);
     }
 
-//    @Override
-//    public void dispose() {
-//        super.dispose();
-//    }
 }
