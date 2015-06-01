61d1bb5c1e3cf77ee4be019a132a4b0305a63ded
miroshn <miroshn11@yandex.ru>
2015-06-01 15:57:48 +0300

    Добавленны комментарии и javadoc

--- core/src/ru/miroshn/cartoon_raider/gameobjects/ui/Titles.java
+++ core/src/ru/miroshn/cartoon_raider/gameobjects/ui/Titles.java
@@ -3,22 +3,32 @@ package ru.miroshn.cartoon_raider.gameobjects.ui;
 import ru.miroshn.cartoon_raider.helpers.Res;
 
 /**
+ * возможные виды заголовков используемых в программе <br>
  * Created by CAHEK on 11.04.2015.
- * возможные виды заголовков
+ * @see Title
  */
 public enum Titles {
+    /**
+     * Название игры
+     */
     WELCOME_TITLE {
         @Override
         public Res getRes() {
             return Res.WELCOME_TITLE;
         }
     },
+    /**
+     * игра закончена
+     */
     GAME_OVER_TITLE {
         @Override
         public Res getRes() {
             return Res.GAME_OVER_TITLE;
         }
     },
+    /**
+     * Пауза
+     */
     GAME_PAUSED_TITLE {
         @Override
         public Res getRes() {
@@ -26,5 +36,11 @@ public enum Titles {
         }
     };
 
+    /**
+     * Получение связанного ресурса
+     *
+     * @return ресурс связанный с конкретным значением перечисления
+     * @see Res
+     */
     public abstract Res getRes();
 }
