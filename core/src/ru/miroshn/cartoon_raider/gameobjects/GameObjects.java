030c476050c09c891ba287ae652a8b36320850d2
miroshn <miroshn11@yandex.ru>
2015-06-02 13:12:13 +0300

    Добавленны комментарии и javadoc

--- core/src/ru/miroshn/cartoon_raider/gameobjects/GameObjects.java
+++ core/src/ru/miroshn/cartoon_raider/gameobjects/GameObjects.java
@@ -1,15 +1,25 @@
 package ru.miroshn.cartoon_raider.gameobjects;
 
 /**
+ * Перечисление возможных типов объектов {@link GameObject}
+ * <br/>
  * Created by miroshn on 17.04.15.
- * Перечисление возможных типов объектов GameObject
  */
 public enum GameObjects {
+    /**
+     * Игрок
+     */
     PLAYER,
+    /** Босс */
     BOSS1,
+    /** Вражеский снаряд */
     ENEMY_BULLET,
+    /** Вражеский истребитель */
     ENEMY_ISTREBITEL,
+    /** Снаряд Игрока */
     PLAYER_BULLET,
+    /** Звезда */
     STAR,
+    /** Ракета */
     ROCKET
 }
